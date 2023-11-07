e7e1171ffac1fa7fde366289a82e5ef22e571984
Estiben Fernandez Echeverri <efernandeze@unal.edu.co>
2023-11-06 19:55:40 -0500

    Suscriber para la velocidad

--- src/RobotState.jsx
+++ src/RobotState.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { RosContext } from "./Context/RosContext";
 import ROSLIB from "roslib/src/RosLib";
 import Config from "./Config/config";
@@ -10,8 +10,18 @@ const { Title, Text } = Typography;
 const RobotState = () => {
   const { ros, conexion } = useContext(RosContext);
   const [posicion, setPosicion] = useState({});
-  const [velocidades, setVelocidades] = useState({})
+  const [velocidades, setVelocidades] = useState({
+    linear : {
+        x:0,
+        y:0
+    },
+    angular : {
+        z:0,
+    }
+  })
   const [orientacion, setOrientacion] = useState({})
+  
+  //Suscriber de la posición y velocidad 
   const getPosicion = () => {
     const pose_suscriber = new ROSLIB.Topic({
       ros: ros,
@@ -27,11 +37,20 @@ const RobotState = () => {
       setOrientacion( {
         orientacion: getOrientacion(message.pose.pose.orientation).toFixed(0)
       })
+
+      setVelocidades( {
+        linear: {
+            x: message.twist.twist.linear.x.toFixed(2),
+            y: message.twist.twist.linear.y.toFixed(2)
+        },
+        angular: {
+            z: message.twist.twist.angular.z.toFixed(2)
+        }
+
+    })
     });
   };
-  useEffect(() => {
-    console.log(orientacion)
-  },[orientacion])
+
   const getOrientacion = (orientation) => {
     const quaternion = new Three.Quaternion(
         orientation.x,
@@ -62,6 +81,16 @@ const RobotState = () => {
 
       <Row>
         <Text>Orientación : {orientacion.orientacion} </Text>
+      </Row>
+
+      <Row>
+        <Title level={5}>Velocidades</Title>
+      </Row>
+      <Row>
+        <Text>Lineal : {velocidades.linear.x}</Text>
+      </Row>
+      <Row>
+        <Text>Angular : {velocidades.angular.z}</Text>
       </Row>
     </>
   );
